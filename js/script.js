e7acc3b18ae706612ceb2be340c321b813c457c6
lizz <elizabeth.ann.soup@gmail.com>
2020-04-29 21:37:40 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Declare Globale variables
+const arrowBtn = document.querySelector('#arrow');
+const landing = document.querySelector('.landing');
+const mainHeader = document.querySelector('.main-header');
+
+// click event listener to arrow link
+arrowBtn.addEventListener('click', hideLanding);
+
+// Hide arrow link after scroll event
+// Shrink landing page and re-style h1 element 
+function hideLanding() {
+  setTimeout(function() {
+    arrowBtn.style.display = 'none';
+    landing.style.justifyContent = 'flex-start';
+    landing.classList.add('shrink');
+    mainHeader.classList.add('header-desktop');
+  }, 1000);
+};
